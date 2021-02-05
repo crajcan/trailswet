ddf7886558d16396aee689c469605c331c860580
Carson Rajcan <carajcan@gmail.com>
2021-02-04 23:23:04 -0600

    Got a real webserver running

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{error, get, post, web, App, Error, HttpResponse, HttpServer, Responder};
+use tera::Tera;
 
 #[get("/")]
-async fn home() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn home(view: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    let mut presenter = tera::Context::new();
+    presenter.insert("name", &"Carson".to_owned());
+
+    let body = view
+        .render("home.html", &presenter)
+        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(body))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(home)
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/views/**/*")).unwrap();
+
+        App::new().data(tera).service(home)
     })
     .bind("127.0.0.1:3000")?
     .run()
