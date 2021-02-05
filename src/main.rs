26ffce23a879f1bbabedd0a57ece9d28b075d8a0
Carson Rajcan <carajcan@gmail.com>
2021-02-04 22:15:40 -0600

    A webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(home)
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
