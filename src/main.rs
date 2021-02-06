b3df207b53841dafebe013ea843a20d291fc1505
Carson Rajcan <carajcan@gmail.com>
2021-02-06 11:58:21 -0600

    Create template presenter from serde serializable struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use actix_web::{error, get, post, web, App, Error, HttpResponse, HttpServer, Responder};
-use tera::Tera;
+use tera::{Tera, Context};
+use serde::Serialize;
 
 #[get("/")]
 async fn home(view: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
-    let mut presenter = tera::Context::new();
-    presenter.insert("name", &"Carson".to_owned());
+    let game = &Context::from_serialize(&GameOrchestrator {
+                home_team_name: "Miami Hurricanes".into(),
+                away_team_name: "Nebraska Cornhuskers".into(),
+            }).unwrap();
 
     let body = view
-        .render("home.html", &presenter)
+        .render("home.html", game)
         .map_err(|_| error::ErrorInternalServerError("Template error"))?;
 
     Ok(HttpResponse::Ok().content_type("text/html").body(body))
+}
+
+#[derive(Serialize)]
+struct GameOrchestrator {
+    home_team_name: String,
+    away_team_name: String,
 }
 
 #[actix_web::main]
