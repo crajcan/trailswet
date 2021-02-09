e1741b4a91a697e5fac5d94047b850ebcffe10ad
Carson Rajcan <carajcan@gmail.com>
2021-02-08 23:35:17 -0600

    Respond to multiple mime types in one route

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,25 @@
-use actix_web::{error, get, post, web, App, Error, HttpResponse, HttpServer, Responder};
+use actix_web::{error, get, web, App, Error, HttpResponse, HttpServer};
 use serde::Serialize;
 use tera::{Context, Tera};
 
-#[get("/home{tail:.*}")]
-async fn home(view: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+#[get("/{tail:.*}")]
+async fn home(path: web::Path<String>, view: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
     let game = GameOrchestrator {
         home_team_name: "Miami Hurricanes".into(),
         away_team_name: "Nebraska Cornhuskers".into(),
     };
 
-    let body = view
-        .render("home.html", &Context::from_serialize(&game).unwrap())
-        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+    match path.as_str() {
+        "" => {
+            let body = view
+                .render("home.html", &Context::from_serialize(&game).unwrap())
+                .map_err(|_| error::ErrorInternalServerError("Template error"))?;
 
-    Ok(HttpResponse::Ok().content_type("text/html").body(body))
-}
-
-#[get("/home_json")]
-async fn home_json() -> Result<HttpResponse, Error> {
-    let game = GameOrchestrator {
-        home_team_name: "Miami Hurricanes".into(),
-        away_team_name: "Nebraska Cornhuskers".into(),
-    };
-
-    Ok(HttpResponse::Ok().json(game))
+            Ok(HttpResponse::Ok().content_type("text/html").body(body))
+        }
+        ".json" => Ok(HttpResponse::Ok().json(game)),
+        _ => Err(error::ErrorNotFound("Resource Not Found")),
+    }
 }
 
 #[derive(Serialize)]
@@ -32,14 +28,13 @@ struct GameOrchestrator {
     away_team_name: String,
 }
 
-
 //try foo/{bar}/{tail:.*} to match /home.json
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/views/**/*")).unwrap();
 
-        App::new().data(tera).service(home).service(home_json)
+        App::new().data(tera).service(home)
     })
     .bind("127.0.0.1:3000")?
     .run()
