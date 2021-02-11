79354b4df23f200f61c441a0fbbe23175c35c3b8
Carson Rajcan <carajcan@gmail.com>
2021-02-10 22:20:55 -0600

    Get a middleware working

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,27 @@
-use actix_web::{error, get, web, App, Error, HttpResponse, HttpServer};
-use serde::Serialize;
-use tera::{Context, Tera};
+use actix_service::Service;
+use actix_web::{App, get, HttpServer};
+use futures::future::FutureExt;
 
-#[get("/{tail:.*}")]
-async fn home(path: web::Path<String>, view: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
-    let game = GameOrchestrator {
-        home_team_name: "Miami Hurricanes".into(),
-        away_team_name: "Nebraska Cornhuskers".into(),
-    };
+#[get("/index.html")]
+async fn index() -> &'static str {
+    println!("in handler");
 
-    match path.as_str() {
-        "" => {
-            let body = view
-                .render("home.html", &Context::from_serialize(&game).unwrap())
-                .map_err(|_| error::ErrorInternalServerError("Template error"))?;
-
-            Ok(HttpResponse::Ok().content_type("text/html").body(body))
-        }
-        ".json" => Ok(HttpResponse::Ok().json(game)),
-        _ => Err(error::ErrorNotFound("Resource Not Found")),
-    }
+    "Welcome!"
 }
 
-#[derive(Serialize)]
-struct GameOrchestrator {
-    home_team_name: String,
-    away_team_name: String,
-}
 
-//try foo/{bar}/{tail:.*} to match /home.json
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/views/**/*")).unwrap();
-
-        App::new().data(tera).service(home)
+        App::new()
+            .wrap_fn(|req, srv| {
+                println!("before handler, path: {}", req.path());
+                srv.call(req).map(|res| {
+                    println!("processing response");
+                    res
+                })
+            })
+            .service(index)
     })
     .bind("127.0.0.1:3000")?
     .run()
