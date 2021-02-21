f5cb4782fb43b833d5dd300cbfdcce7dace0529c
Carson Rajcan <carajcan@gmail.com>
2021-02-20 21:30:21 -0600

    Move Presenter out of main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use actix_http::{Error, Response};
 use actix_web::*;
-use futures_util::future::{err, ok, Ready};
 use serde::Serialize;
-use tera::{Context, Tera};
+use tera::Tera;
+
+mod utils;
+use utils::presenter::Presenter;
 
 #[derive(Serialize)]
 struct GameOrchestrator {
@@ -20,35 +21,6 @@ async fn index() -> impl Responder {
     Presenter {
         resource: orchestrator,
         template: "index.html",
-    }
-}
-
-struct Presenter<R: Serialize, T: Into<String>> {
-    resource: R,
-    template: T,
-}
-
-impl<R: Serialize, T: Into<String>> Responder for Presenter<R, T> {
-    type Error = Error;
-    type Future = Ready<Result<Response, Error>>;
-
-    fn respond_to(self, req: &HttpRequest) -> Self::Future {
-        let mime = req.match_info().query("tail");
-        let view = req.app_data::<web::Data<tera::Tera>>().unwrap();
-
-        match mime {
-            "" | "/" => {
-                let body = view
-                    .render(
-                        &self.template.into(),
-                        &Context::from_serialize(&self.resource).unwrap(),
-                    )
-                    .unwrap();
-                ok(HttpResponse::Ok().content_type("text/html").body(body))
-            }
-            ".json" => ok(HttpResponse::Ok().json(&self.resource)),
-            _ => err(error::ErrorNotFound("Resource Not Found")),
-        }
     }
 }
 
