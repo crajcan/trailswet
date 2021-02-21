cbd55c8534e8d28991d0e97d5b8ed11421bb74ab
Carson Rajcan <carajcan@gmail.com>
2021-02-20 23:23:56 -0600

    Create controllers

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,18 @@
 use actix_web::*;
-use serde::Serialize;
 use tera::Tera;
 
 mod utils;
 use utils::presenter::Presenter;
 
-#[derive(Serialize)]
-struct GameOrchestrator {
-    home_team_name: String,
-    away_team_name: String,
-}
-
-#[get("/index{tail:.*}")]
-async fn index() -> impl Responder {
-    let orchestrator = GameOrchestrator {
-        home_team_name: "Miami Hurricanes".into(),
-        away_team_name: "Nebraska Cornhuskers".into(),
-    };
-
-    Presenter {
-        resource: orchestrator,
-        template: "index.html",
-    }
-}
+mod controllers;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    use controllers::*;
+
     HttpServer::new(|| {
         let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/views/**/*")).unwrap();
-        App::new().data(tera).service(index)
+        App::new().data(tera).service(games_controller::show)
     })
     .bind("127.0.0.1:3000")?
     .run()
@@ -44,8 +28,8 @@ mod tests {
     #[actix_rt::test]
     async fn test_json_get() {
         let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/views/**/*")).unwrap();
-        let mut app = test::init_service(App::new().data(tera).service(index)).await;
-        let req = test::TestRequest::with_uri("/index.json").to_request();
+        let mut app = test::init_service(App::new().data(tera).service(show)).await;
+        let req = test::TestRequest::with_uri("/show.json").to_request();
         let mut resp = test::call_service(&mut app, req).await;
         let body = resp.take_body();
         let body = body.as_ref().unwrap();
@@ -60,8 +44,8 @@ mod tests {
     /// Template doesnt exist
     async fn test_web_get() {
         let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/views/**/*")).unwrap();
-        let mut app = test::init_service(App::new().data(tera).service(index)).await;
-        let req = test::TestRequest::with_uri("/index").to_request();
+        let mut app = test::init_service(App::new().data(tera).service(show)).await;
+        let req = test::TestRequest::with_uri("/show").to_request();
         let _resp = test::call_service(&mut app, req).await;
     }
 }
