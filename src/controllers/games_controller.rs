7a5b5e8ca69a8b1a049c0b6b2ade3a26298d9433
Carson Rajcan <carajcan@gmail.com>
2021-02-21 11:43:56 -0600

    Make a thing worth orchestrating

--- src/controllers/games_controller.rs
+++ src/controllers/games_controller.rs
@@ -1,18 +1,44 @@
+use crate::Presenter;
 use actix_web::*;
 use serde::Serialize;
-use crate::Presenter;
+
+#[derive(Serialize)]
+struct Game {
+    home_team_id: u16,
+    away_team_id: u16,
+}
+
+#[derive(Serialize)]
+struct Team {
+    id: u16,
+    name: String,
+    url: String,
+}
 
 #[derive(Serialize)]
 struct GameOrchestrator {
-    home_team_name: String,
-    away_team_name: String,
+    game: Game,
+    home_team: Team,
+    away_team: Team,
 }
 
 #[get("/show{tail:.*}")]
 async fn show() -> impl Responder {
     let orchestrator = GameOrchestrator {
-        home_team_name: "Miami Hurricanes".into(),
-        away_team_name: "Nebraska Cornhuskers".into(),
+        game: Game {
+            home_team_id: 1,
+            away_team_id: 2,
+        },
+        home_team: Team {
+            id: 1,
+            name: "Miami Hurricanes".into(),
+            url: "http://localhost:3000/teams/1".into(),
+        },
+        away_team: Team {
+            id: 2,
+            name: "Nebraska Cornhuskers".into(),
+            url: "http://localhost:3000/teams/2".into(),
+        },
     };
 
     Presenter {
