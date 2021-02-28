80c9f3c7cc370f6c5b6f425d274737e128098c0c
Carson Rajcan <carajcan@gmail.com>
2021-02-28 16:51:59 -0600

    Got an error that works

--- src/controllers/games_controller.rs
+++ src/controllers/games_controller.rs
@@ -1,16 +1,36 @@
 use crate::game_orchestrator::GameOrchestrator;
 use crate::Presenter;
 use actix_web::*;
+use actix_web::web::{Path, Data};
 use serde::Deserialize;
-use sqlx::PgPool;
+use sqlx::{PgPool};
+use derive_more::{Display, Error};
+use std::convert::From;
+
 
 #[derive(Deserialize)]
 struct PathParams {
     id: i32,
 }
 
+#[derive(Debug, Display, Error)]
+#[display(fmt = "my error: {}", name)]
+struct NetflixError {
+    name: String,
+}
+
+impl error::ResponseError for NetflixError {}
+
+impl From<sqlx::Error> for NetflixError {
+    fn from(e: sqlx::Error) -> Self {
+        let name = e.to_string();
+
+        NetflixError { name: name }
+    }
+}
+
 #[get(r#"/show/{id:\d+}{tail:.*}"#)]
-async fn show(path_params: web::Path<PathParams>, db_pool: web::Data<PgPool>) -> Result<impl Responder> {
+async fn show(path_params: Path<PathParams>, db_pool: Data<PgPool>) -> Result<impl Responder, NetflixError> {
     let orchestrator = GameOrchestrator::find(path_params.id, db_pool.get_ref()).await;
 
     match orchestrator {
@@ -19,6 +39,9 @@ async fn show(path_params: web::Path<PathParams>, db_pool: web::Data<PgPool>) ->
                           resource: data,
                       })
         },
-        Err(_) => Err(error::ErrorNotFound(path_params.id))
+        Err(e) => {
+            Err(NetflixError::from(e))
+        }
     }
 }
+
